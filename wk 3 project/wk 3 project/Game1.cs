65f8b808f13405e193f7b10afa168913a795aa98
liamhamilton <43400952+liamhamilton@users.noreply.github.com>
2018-09-19 18:02:15 +1000

    Revert "Revert "initial commit of wk 3 project""
    This reverts commit 0bd9f62bf2733efa040398bf2cd05d2b229310c9.

---
+++ wk 3 project/wk 3 project/Game1.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace wk_3_project
+{
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+
+		Class2 player = new Class2();
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+		}
+
+		
+		protected override void Initialize()
+		{
+			
+
+			base.Initialize();
+		}
+
+		
+		protected override void LoadContent()
+		{
+			
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			player.Load(Content);
+		}
+
+		
+		protected override void UnloadContent()
+		{
+			
+		}
+
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+			player.Update(deltaTime);
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			spriteBatch.Begin();
+			player.Draw(spriteBatch);
+			spriteBatch.End(); 
+
+			base.Draw(gameTime);
+		}
+	}
+}
